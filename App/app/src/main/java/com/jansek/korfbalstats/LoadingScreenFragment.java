4eb7a437e07c28668c9845dce4dac40d35c17ed2
C-Jansek <mailcjanssen@gmail.com>
2019-05-22 12:50:26 +0200

    Add fonts

---
+++ App/app/src/main/java/com/jansek/korfbalstats/LoadingScreenFragment.java
@@ -0,0 +1,21 @@
+package com.jansek.korfbalstats;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class LoadingScreenFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.ks_loading_screen_fragment, container, false);
+
+        return view;
+    }
+}
--- App/app/src/main/java/com/jansek/korfbalstats/MainActivity.java
+++ App/app/src/main/java/com/jansek/korfbalstats/MainActivity.java
@@ -1,13 +1,22 @@
 package com.jansek.korfbalstats;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new LoadingScreenFragment())
+                    .commit();
+        }
     }
 }
