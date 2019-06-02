0992f9ae44044417e67c7486adc70a916a75f429
C-Jansek <mailcjanssen@gmail.com>
2019-06-02 21:47:48 +0200

    Loadingscreen

--- App/app/src/main/java/com/jansek/korfbalstats/LoadingScreenFragment.java
+++ App/app/src/main/java/com/jansek/korfbalstats/LoadingScreenFragment.java
@@ -1,5 +1,6 @@
 package com.jansek.korfbalstats;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
 
 public class LoadingScreenFragment extends Fragment {
 
@@ -16,6 +19,18 @@ public class LoadingScreenFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.ks_loading_screen_fragment, container, false);
 
+        configureContinueButton();
         return view;
+
+    }
+
+    private void configureContinueButton() {
+        MaterialButton continueButton = getView().findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoadingscreenFragment.this, MatchActivity.class));
+            }
+        });
     }
 }
--- App/app/src/main/java/com/jansek/korfbalstats/MatchActivity.java
+++ App/app/src/main/java/com/jansek/korfbalstats/MatchActivity.java
@@ -6,22 +6,19 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MatchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.match_activity);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .add(R.id.container, new LoadingScreenFragment())
+                    .add(R.id.container, new MatchFragment())
                     .commit();
         }
-
-
-
     }
 }
